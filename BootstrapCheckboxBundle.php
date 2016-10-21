288f45ca80bb2e40d35e3a6f1dce4fb49a592727
Janusz Paszyński <eloar@itchaos.pl>
2016-10-22 00:12:40 +0200

    * Basic Bundle Structure * Readme File * composer dependencies * .gitignore

---
+++ BootstrapCheckboxBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the BootstrapCheckboxBundle.
+ *
+ * (c) Janusz Paszyński <https://github.com/Eloar>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ITChaos\Bundle\BootstrapCheckboxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BootstrapCheckboxBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+Bootstrap Checkbox Bundle
+=========================
+
+This is just simple wraping bundle for Bootstrap Checkbox.
+
+Requirements
+------------
+* php 5.6
+* symfony 2.3
+
+Instalation
+-----------
+
+1. Add to composer requirements
+2. Register bundle in Your AppKernel
